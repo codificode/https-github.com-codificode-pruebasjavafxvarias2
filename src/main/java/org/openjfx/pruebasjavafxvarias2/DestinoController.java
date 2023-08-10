65913367064a76fe342996c7fada759c81ffc1c1
rubic <codificode@gmail.com>
2023-08-10 06:35:35 +0200

    Segundo método de comunicación entre clases en javafx. Funciona.

--- src/main/java/org/openjfx/pruebasjavafxvarias2/DestinoController.java
+++ src/main/java/org/openjfx/pruebasjavafxvarias2/DestinoController.java
@@ -6,7 +6,10 @@ package org.openjfx.pruebasjavafxvarias2;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -14,13 +17,32 @@ import javafx.fxml.Initializable;
  * @author rubic
  */
 public class DestinoController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    
+    private Usuario usuario;
+    
+    public void setUsuario (Usuario usuario){
+        this.usuario = usuario;
+    }
+    
+    @FXML
+    private Label lblNombre, lblCiudad, lblApellido;
+    
+    @FXML
+    private Button boton;
+    
+    public Button getBoton() {
+        return boton;
+    }
+    
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    public void initialize(URL url, ResourceBundle rb) { 
     }    
     
+    @FXML
+    public String recuperarDatos() {
+        lblNombre.setText("Nombre: " + this.usuario.getNombre());
+        lblCiudad.setText("Ciudad: " + this.usuario.getCiudad());
+        lblApellido.setText("Apellido: " + this.usuario.getApellido());
+        return "Hola";
+    }    
 }
--- src/main/java/org/openjfx/pruebasjavafxvarias2/PrimaryController.java
+++ src/main/java/org/openjfx/pruebasjavafxvarias2/PrimaryController.java
@@ -3,10 +3,19 @@ package org.openjfx.pruebasjavafxvarias2;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class PrimaryController implements Initializable {
     
@@ -16,7 +25,7 @@ public class PrimaryController implements Initializable {
     private TextField tfNombre, tfApellido, tfCiudad;
     
     @FXML
-    private void enviarDatos(MouseEvent event) {
+    private void enviarDatos() {
         String nombre = tfNombre.getText().trim();
         String apellido = tfApellido.getText().trim();
         String ciudad = tfCiudad.getText().trim();
@@ -24,12 +33,44 @@ public class PrimaryController implements Initializable {
         u.setNombre(nombre);
         u.setApellido(apellido);
         u.setCiudad(ciudad);
+        
+        Node node = tfNombre;
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+        
+        try{
+            FXMLLoader fxmlLoader = new FXMLLoader(PrimaryController.class.getResource("Destino.fxml"));
+            
+            DestinoController controller = new DestinoController();
+            controller.setUsuario(u);
+            fxmlLoader.setController(controller);
+            Parent root = fxmlLoader.load();
+            Scene scene = new Scene(root);
+            
+            
+            Button boton=controller.getBoton();
+            
+            boton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    controller.recuperarDatos();
+                }
+            });
+            
+            
+
+            stage.setTitle("Tutorial javafx");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e){
+            System.err.println("Error creando ventana");
+        }
     }
     
     
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 }
